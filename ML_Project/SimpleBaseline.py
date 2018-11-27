2a562903e515c615b97b66e0ba5a7a74cc372cb2
tom0396 <tom.linker@hotmail.co.uk>
2018-11-27 13:21:28 +0000

    added to tom branch

--- ML_Project/SimpleBaseline.py
+++ ML_Project/SimpleBaseline.py
@@ -18,6 +18,15 @@ def main():
 	        missingI.append(i)
 	housing.drop(missingI,axis=0,inplace=True)
 	
+	
+	match = best_fit('total_bedrooms', housing)
+	print("Best match  for total_bedrooms is: ")
+	print(match)
+	
+	
+	
+	
+	
 	y = housing.median_house_value.values.reshape(-1,1)
 	x = housing.drop(columns=['median_house_value'], inplace=False).values
 	
@@ -46,8 +55,7 @@ def k_fold_train(Model, k, x, y):
 	    trainingSum += trainingError
 	    testingSum += testingError
 	    
-	    print('Training error: ' + str(trainingError))
-	    print('Testing error: ' + str(testingError))
+	    
 
 	trainingAvg = trainingSum/k
 	testingAvg = testingSum/k
@@ -57,8 +65,70 @@ def k_fold_train(Model, k, x, y):
 	
 	
 	
+def replace_missing():
+	#fields:
+	#longitude
+	#latitude
+	#housing_median_age
+	#total_rooms
+	#total_bedrooms
+	#population
+	#households
+	#median_income
+	#households
+	#median_house_value
+	#ocean_proximity
 	
+	
+	return 0
+	
+def best_fit(column, housing):
+	types = list(housing)
+	types.remove(column)
+	print(types)
+	
+	x = housing[column].values.reshape(-1,1)
+	k = 5
+	
+	bestFit = 10000
+	bestMatch = "none"
+	Model = lm.LinearRegression()
+	
+	#TODO return the model to predict stuff
+	bestModel = lm.LinearRegression()
+	
+	
+	
+	for i in range(len(types)):
+		y = housing[types[i]].values.reshape(-1,1)
+		Model, trainingAvg, testingAvg = k_fold_train(Model, k, x, y)
+		if bestFit > testingAvg:
+			bestFit = testingAvg
+			bestMatch = types[i]
+			
+		print("Fit (testingAvg) between:")
+		print(column)	
+		print(types[i])
+		print(testingAvg)
+		print('\n')
+		
+	
+	return bestMatch
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__": main()
+
+
+
+
+
+
 
